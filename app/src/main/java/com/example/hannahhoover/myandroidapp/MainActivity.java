cca4166403dd066238cc79627327e7144872ad9d
Hannah Hoover <hannahghoover@gmail.com>
2018-03-27 17:05:56 -0500

    Initial commit.

---
+++ app/src/main/java/com/example/hannahhoover/myandroidapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.hannahhoover.myandroidapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        //setSupportActionBar(myToolbar);
+    }
+}
